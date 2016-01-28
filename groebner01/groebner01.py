b94296fc658a974b3eac38a07340016c7b8b38c3
EisakuHiguchi <13ec083@ms.dendai.ac.jp>
2016-01-28 12:38:54 +0900

    2つのNNをまとめる実験

--- groebner01/groebner01.py
+++ groebner01/groebner01.py
@@ -34,20 +34,41 @@ def func_A(G):
         print(',\n'.join(map(str,solve(Eq(e,0)))))
 
 def genW(n):
-    W1 = Matrix([ \
+    if n == "XOR1":
+        return Matrix([ \
 		[3.382219091542679, -0.8469657102458379, -0.503925721263563], \
 		[4.041814394745013, 4.619087381978036, -0.6558632685417835], \
 		[-2.5098945335589846, 4.22870944209639, 1.0490917396825894] \
 		])
 
-    W2 = Matrix([ \
+    elif n == "XOR2":
+        return Matrix([ \
         [-3.453323154233995, 5.376950275446613, -3.517647020953983] ,\
         ])
+    
+    elif n == "NAND1":
+        return Matrix([ \
+        [-1.1361701010131928, -1.337147014979442, 1.2024365070791911],\
+        [-2.1470710158957416, -1.8110808604245894, 2.280096007160183],\
+        [0.23176061517419683, 0.9630548121645178, 0.9849504981704243]
+		])
+    
+    elif n == "NAND2":
+        return Matrix([ \
+        [1.4445008439861187, 3.6836311164057065, -1.6520525509333777]\
+        ])
+    
+    elif n == "OR1":
+        return Matrix([\
+        [2.4814976740913455, 2.6289388891403647, -1.724995854522851],\
+        [0.15970864442913618, 0.9967219490017045, 0.9748714142772889],\
+        [2.1901016295197064, 2.004980233004023, -1.4715097824241157]\
+        ])
 
-    if n == 1:
-        return W1
-    else:
-        return W2
+    elif n == "OR2":
+        return Matrix([\
+        [2.734737045787183, -1.6577183008594898, 1.8271790744144403]\
+        ])
     
 def activateSub(value):
     return 1.0 / (1.0 + exp(-1 * value)) 
@@ -63,9 +84,9 @@ def activateFunc(A):
     return Matrix(result)
 
 
-def func1(i0 , i1):
-    W1 = genW(1)
-    W2 = genW(2)
+def func1(i0 , i1, B):
+    W1 = genW(B + str(1))
+    W2 = genW(B + str(2))
 
     #w03,w13,w23,w04,w14,w24,w05,w15,w25,w36,w46,w56 = symbols('w03 w13 w23 w04 w14 w24 w05 w15 w25 w36 w46 w56')
     #W1 = Matrix([\
@@ -90,67 +111,79 @@ def func1(i0 , i1):
     Ga2 = groebner(activateFunc(A2))
     
     #A3 = A2.subs([[x0, i0], [x1, i1], [x2, 1]])
-    A3 = A2.subs([[x0, i0], [x2, 1]])
-    G3 = groebner(A3)
-    Ga3 = groebner(activateFunc(A3))
+    #A3 = A2.subs([[x0, i0], [x2, 1]])
+    #G3 = groebner(A3)
+    #Ga3 = groebner(activateFunc(A3))
     
+    return activateFunc(A2)
 
     # record
-    print("\nA1 = W1 * X1\n")
-    print(A1)
-    print("\nG1 = groebner(A1)\n")
-    print(G1)
-    print("\nGa1 = groebner(activate(A1))\n")
-    print(Ga1)
-    print("\nA1 = W2 * activate(A1)\n")
-    print(A2)
-    print("\nG2 = groebner(A2)\n")
-    print(G2)
-    print("\nGa2 = groebner(activate(A2))\n")
-    print(Ga2)
-    print("\nA3 : solve A2\n")
-    print(A3)
-    print("\nG3 = groebner(A3)\n")
-    print(G3)
-    print("\nGa3 = groebner(activate(A3)\n")
-    print(Ga3)
-    print("\noutput = activate(A3)\n")
-    print(activateFunc(A3))
+    #print("\nA1 = W1 * X1\n")
+    #print(A1)
+    #print("\nG1 = groebner(A1)\n")
+    #print(G1)
+    #print("\nGa1 = groebner(activate(A1))\n")
+    #print(Ga1)
+    #print("\nA1 = W2 * activate(A1)\n")
+    #print(A2)
+    #print("\nG2 = groebner(A2)\n")
+    #print(G2)
+    #print("\nGa2 = groebner(activate(A2))\n")
+    #print(Ga2)
+    #print("\nA3 : solve A2\n")
+    #print(A3)
+    #print("\nG3 = groebner(A3)\n")
+    #print(G3)
+    #print("\nGa3 = groebner(activate(A3)\n")
+    #print(Ga3)
+    #print("\noutput = activate(A3)\n")
+    #print(activateFunc(A3))
     
-    f = open("c:\work\Data\groebner01_" + datetime.datetime.today().strftime(("%Y%m%d%H%M%S")) + ".txt","w")
+    #f = open("c:\work\Data\groebner01_" + datetime.datetime.today().strftime(("%Y%m%d%H%M%S")) + ".txt","w")
 
-    f.write("data\n")
-    f.write("W1\n")
-    f.write(' , '.join(map(str, W1.tolist())))
-    f.write("W2\n")
-    f.write(' , '.join(map(str, W2.tolist())))
-    f.write("X1\n")
-    f.write(' , '.join(map(str, X1.tolist())))
-    f.write("X2\n")
-    f.write(' , '.join(map(str, X2.tolist())))
+    #f.write("data\n")
+    #f.write("W1\n")
+    #f.write(' , '.join(map(str, W1.tolist())))
+    #f.write("W2\n")
+    #f.write(' , '.join(map(str, W2.tolist())))
+    #f.write("X1\n")
+    #f.write(' , '.join(map(str, X1.tolist())))
+    #f.write("X2\n")
+    #f.write(' , '.join(map(str, X2.tolist())))
 
-    f.write("\n\n\nA1 = W1 * X1\n")
-    f.write(' , '.join(map(str, A1.tolist())))
-    f.write("\n\nG1 = groebner(A1)\n")
-    f.write(' , '.join(map(str, G1.exprs)))
-    f.write("\n\nGa1 = groebner(activate(A1))\n")
-    f.write(' , '.join(map(str, Ga1.exprs)))
-    f.write("\n\nA1 = W2 * activate(A1)\n")
-    f.write(' , '.join(map(str, A2.tolist())))
-    f.write("\n\nG2 = groebner(A2)\n")
-    f.write(' , '.join(map(str, G2.exprs)))
-    f.write("\n\nGa2 = groebner(activate(A2))\n")
-    f.write(' , '.join(map(str, Ga2.exprs)))
-    f.write("\n\nA3 : solve A2\n")
-    f.write(' , '.join(map(str, A3.tolist())))
-    f.write("\n\nG3 = groebner(A3)\n")
-    f.write(' , '.join(map(str, G3.exprs)))
-    f.write("\n\nGa3 = groebner(activate(A3)\n")
-    f.write(' , '.join(map(str, Ga3.exprs)))
-    f.write("\n\noutput = activate(A3)\n")
-    f.write(' , '.join(map(str, activateFunc(A3).tolist())))
+    #f.write("\n\n\nA1 = W1 * X1\n")
+    #f.write(' , '.join(map(str, A1.tolist())))
+    #f.write("\n\nG1 = groebner(A1)\n")
+    #f.write(' , '.join(map(str, G1.exprs)))
+    #f.write("\n\nGa1 = groebner(activate(A1))\n")
+    #f.write(' , '.join(map(str, Ga1.exprs)))
+    #f.write("\n\nA1 = W2 * activate(A1)\n")
+    #f.write(' , '.join(map(str, A2.tolist())))
+    #f.write("\n\nG2 = groebner(A2)\n")
+    #f.write(' , '.join(map(str, G2.exprs)))
+    #f.write("\n\nGa2 = groebner(activate(A2))\n")
+    #f.write(' , '.join(map(str, Ga2.exprs)))
+    #f.write("\n\nA3 : solve A2\n")
+    #f.write(' , '.join(map(str, A3.tolist())))
+    #f.write("\n\nG3 = groebner(A3)\n")
+    #f.write(' , '.join(map(str, G3.exprs)))
+    #f.write("\n\nGa3 = groebner(activate(A3)\n")
+    #f.write(' , '.join(map(str, Ga3.exprs)))
+    #f.write("\n\noutput = activate(A3)\n")
+    #f.write(' , '.join(map(str, activateFunc(A3).tolist())))
+
+
 
     
 if __name__ == "__main__":
     #func([x1 + x2 + 4] + [x1**2 + x2**2])
-    func1(0,0)
+    G1 = func1(0,0, "NAND")
+    G2 = func1(0,0, "OR")
+
+    Gl1 = G1.tolist()
+    Gl2 = G2.tolist()
+    Gl1.append(Gl2)
+
+    print(groebner(G1))
+
+    #print(simplify(func1(0,0, "NAND"))) # bad result
